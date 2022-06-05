fb5a8d2218e8832f0a6b86bd8c4ba30cb1deac25
YunJiye <yjo3440@naver.com>
2022-06-05 21:35:05 +0900

    필요파일 및 user List 조회 함수 생성

---
+++ src/main/java/DataVo/UserInfoVo.java
@@ -0,0 +1,28 @@
+package DataVo;
+
+import java.sql.Timestamp;
+
+public class UserInfoVo {
+	public String userId;
+	public String nickname;
+	public Timestamp registerDate;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public Timestamp getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(Timestamp registerDate) {
+		this.registerDate = registerDate;
+	}
+}
