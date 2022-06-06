cc8aeb8d578854fa7a6516a54ab5e6c2981a1deb
Jenn Kim <grdd0419@gmail.com>
2022-06-07 02:06:56 +0900

    :bug: application에서 DAO 패키지를 인식 못하는 오류 수정(루트 경로 재설정)

--- src/main/java/anft/demo/DemoApplication.java
+++ src/main/java/anft/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package anft.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"java"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
