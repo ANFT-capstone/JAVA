59f4688c844be408881bc0613abfae2f6884cc44
Jenn Kim <grdd0419@gmail.com>
2022-06-08 13:07:51 +0900

    :bookmark: User 전체 조회, id로 조회, 추가 기능 설정. history update 기능 수정중

---
+++ src/main/java/anft/demo/api/controller/UserController.java
@@ -0,0 +1,42 @@
+package anft.demo.api.controller;
+
+import DataVo.UserInfoVo;
+import anft.demo.api.service.NFTService;
+import anft.demo.api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    // 유저 전체 목록 조회
+    public List<UserInfoVo> userList() {
+        return userService.findAll();
+    }
+
+    // 유저 id로 조회
+    public UserInfoVo findByUserId(String userId) {
+
+        return userService.findByUserId(userId);
+    }
+
+    // 새로운 유저 정보 DB에 저장
+    @Transactional
+    public void addUser(String userId, String nickname) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyMMdd");
+        String registerDate = today.format(dateTimeFormatter);
+
+        userService.addUser(userId, nickname, registerDate);
+    }
+}
