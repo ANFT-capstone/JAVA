51e2123674588ccf795f25919064aad09a8d2849
eolgaeng-i <46189116+eolgaeng-i@users.noreply.github.com>
2022-06-25 21:31:52 +0900

    :pencil:nft생성시 DB에 저장되는 URI값 변경

--- NFTmethods/callMethod.js
+++ NFTmethods/callMethod.js
@@ -1277,7 +1277,7 @@ async function airDropMint(URI, usr, cnt) {
       label:result.transactionHash,
       category:'image', // TODO:현재는 그림이미지만 nft화 시킨다는 가정
       price: 0, // TODO: 거래 기능 구현 시 수정 필요
-      uri:result.blockHash, 
+      uri:URI, 
       isSell:'no',
       createUser: result.from,
     })
